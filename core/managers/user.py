6da44b1d0f8a43181a65c758deb29d434228e4d4
Jakub Dubec <jakub.dubec@gmail.com>
2019-03-07 14:43:00 +0100

    Custom user manager

--- core/managers/user.py
+++ core/managers/user.py
@@ -4,4 +4,26 @@ from core.managers.base import BaseManager
 
 
 class UserManager(BaseUserManager, BaseManager):
-    pass
+    use_in_migrations = True
+
+    def _create_user(self, **extra_fields):
+        if extra_fields.get('email') is not None:
+            extra_fields.setdefault('email', self.normalize_email(extra_fields.get('email')))
+
+        user = self.model(**extra_fields)
+        user.set_password(extra_fields.get('password'))
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(**extra_fields)
+
+    def create_superuser(self, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(**extra_fields)
--- core/models/user.py
+++ core/models/user.py
@@ -17,15 +17,15 @@ class User(BaseModel, AbstractBaseUser, PermissionsMixin):
     surname = models.CharField(max_length=50)
 
     USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
 
     objects = UserManager()
+    objects_all = UserManager(alive_only=False)
 
-    @property
-    def full_name(self) -> str:
+    def get_full_name(self) -> str:
         return f'{self.name} {self.surname}'
 
-    @property
-    def short_name(self) -> str:
+    def get_short_name(self) -> str:
         return self.name
 
     def __str__(self):
