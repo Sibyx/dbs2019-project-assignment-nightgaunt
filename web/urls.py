0d678600b7713f4bd47ef04461e1d61d3ef16109
Jakub Dubec <jakub.dubec@gmail.com>
2019-03-31 19:02:47 +0200

    Boxes overview

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,9 @@ from web.views import dashboard, boxes, specimens
 
 urlpatterns = [
     path('dashboard/', dashboard.index, name='dashboard'),
+
     path('boxes/', boxes.overview, name='boxes-overview'),
+    path('boxes/detail/<uuid:id>/', boxes.detail, name='boxes-detail'),
+
     path('specimens/', specimens.overview, name='specimens-overview'),
 ]
--- web/views/boxes.py
+++ web/views/boxes.py
@@ -1,7 +1,33 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse
+
+from core.models import Box
 
 
 @login_required
 def overview(request):
+    if request.is_ajax():
+        boxes = Box.objects.all()
+        response = {
+            'rows': [],
+            'total': len(boxes)
+        }
+
+        for box in boxes:
+            response['rows'].append({
+                "id": box.id,
+                "title": box.title,
+                "description": box.description,
+                "size": box.specimen_set.count(),
+                "detail-url": reverse('boxes-detail', None, [box.id])
+            })
+
+        return JsonResponse(response)
+
     return render(request, 'boxes/overview.html')
+
+
+def detail(request, id):
+    return render(request, 'boxes/detail.html')
