70a0c273f07506cc25af73c2b55f90c328f05123
Jakub Dubec <jakub.dubec@gmail.com>
2019-03-31 17:04:57 +0200

    Bootstrap tables setup (I really hate webpack and jQuery, viva la WebAssembly)

--- web/assets/scripts/app.js
+++ web/assets/scripts/app.js
@@ -8,5 +8,7 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+// Bootstrap table
+import 'bootstrap-table'
 // Project
 import '../sass/app.scss'
