50838ce419fef8128ca1ddb36f9c9fbcbfce1d59
Jakub Dubec <jakub.dubec@gmail.com>
2019-04-07 13:52:37 +0200

    User navigation dropdown in navbar Logout url configuration

--- settings/base.py
+++ settings/base.py
@@ -122,7 +122,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'core.User'
 
-LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from web.views import dashboard, boxes, specimens
 
 urlpatterns = [
-    path('dashboard/', dashboard.index, name='dashboard'),
+    path('', dashboard.index, name='dashboard'),
 
     path('boxes/', boxes.overview, name='boxes-overview'),
     path('boxes/detail/<uuid:id>/', boxes.detail, name='boxes-detail'),
