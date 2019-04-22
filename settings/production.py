445c42433d5279ef86b47270204457bab472fa63
Jakub Dubec <jakub.dubec@gmail.com>
2019-04-22 16:01:01 +0200

    Sentry

--- settings/production.py
+++ settings/production.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from settings.base import *
 
 DEBUG = False
@@ -7,3 +10,8 @@ TIME_ZONE = 'UTC'
 ALLOWED_HOSTS = [
     'mdns.jakubdubec.me'
 ]
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DSN'),
+    integrations=[DjangoIntegration()]
+)
