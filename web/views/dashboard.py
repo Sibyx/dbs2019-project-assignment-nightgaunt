35887f602f1f983b511f9da245a84da4993ec3b2
Jakub Dubec <jakub.dubec@gmail.com>
2019-04-27 11:52:43 +0200

    Dashboard basic stats

--- web/views/dashboard.py
+++ web/views/dashboard.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from core.models import Box, Specimen, Photo, User
+
 
 @login_required
 def index(request):
-    return render(request, 'dashboard/index.html')
+    stats = {
+        'boxes': Box.objects.count(),
+        'specimens': Specimen.objects.count(),
+        'organisms': Specimen.objects.distinct('organism').count(),
+        'photos': Photo.objects.count(),
+        'users': User.objects.count()
+    }
+
+    context = {
+        'stats': stats
+    }
+
+    return render(request, 'dashboard/index.html', context)
